b5bdc58fd2c8a77d3a73a38400c28e3b154dc45c
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-05-20 19:04:43 +0200

    Refactor controller

--- server.js
+++ server.js
@@ -3,6 +3,37 @@ const cors = require('cors');
 const morgan = require('morgan');
 const { pick } = require('lodash');
 
+function initCommuneFields(req, res, next) {
+  if (req.query.fields) {
+    req.fields = new Set(req.query.fields.split(','));
+  } else {
+    req.fields = new Set(['nom', 'codeInsee', 'codesPostaux', 'centre', 'surface']);
+  }
+  req.fields.add('codeInsee', 'nom');
+  next();
+}
+
+function initCommuneFormat(req, res, next) {
+  req.outputFormat = ['json', 'geojson'].indexOf(req.query.format) >= 0 ? req.query.format : 'json';
+  if (req.outputFormat === 'geojson') {
+    req.fields.delete('contour');
+    req.fields.delete('centre');
+  }
+  next();
+}
+
+function formatCommune(req, commune) {
+  if (req.outputFormat === 'geojson') {
+    const geom = ['contour', 'centre'].indexOf(req.query.geometry) >= 0 ? req.query.geometry : 'centre';
+    return {
+      type: 'Feature',
+      properties: pick(commune, Array.from(req.fields)),
+      geometry: commune[geom]
+    };
+  } else {
+    return pick(commune, Array.from(req.fields));
+  }
+}
 
 require('./lib/communeStore')()
   .then(db => {
@@ -10,21 +41,13 @@ require('./lib/communeStore')()
     app.use(cors());
     app.use(morgan('dev'));
 
-    app.get('/communes', function (req, res) {
+    app.get('/communes', initCommuneFields, initCommuneFormat, function (req, res) {
       let result;
-      let fields;
-      const format = ['json', 'geojson'].indexOf(req.query.format) >= 0 ? req.query.format : 'json';
-      if (req.query.fields) {
-        fields = new Set(req.query.fields.split(','));
-      } else {
-        fields = new Set(['nom', 'codeInsee', 'codesPostaux', 'centre', 'surface']);
-      }
-      fields.add('codeInsee', 'nom');
 
       if (req.query.lat && req.query.lon) {
         result = db.query([parseFloat(req.query.lon), parseFloat(req.query.lat)]);
       } else if (req.query.nom) {
-        fields.add('_score');
+        req.fields.add('_score');
         result = db.queryByName(req.query.nom);
       } else if (req.query.codePostal) {
         result = db.queryByCP(req.query.codePostal);
@@ -32,20 +55,13 @@ require('./lib/communeStore')()
         return res.sendStatus(400);
       }
 
-      if (format === 'geojson') {
-        const geom = ['contour', 'centre'].indexOf(req.query.geometry) >= 0 ? req.query.geometry : 'centre';
-        fields.delete('contour');
-        fields.delete('centre');
+      if (req.outputFormat === 'geojson') {
         res.send({
           type: 'FeatureCollection',
-          features: result.map(commune => ({
-            type: 'Feature',
-            properties: pick(commune, Array.from(fields)),
-            geometry: commune[geom]
-          }))
+          features: result.map(commune => formatCommune(req, commune))
         });
       } else {
-        res.send(result.map(commune => pick(commune, Array.from(fields))));
+        res.send(result.map(commune => formatCommune(req, commune)));
       }
     });
 
