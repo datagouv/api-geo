c4f95ca4a5da0269741f4265b77efac0b4af896a
tmerlier <tmerlier@student.42.fr>
2016-05-31 20:11:37 +0200

    eslint off

--- integration.js
+++ integration.js
@@ -1,10 +1,12 @@
+/* eslint no-console: ["off"] */
 const integrate = require('./lib/integrate').integration;
 const debug = require('debug')('integrate');
 
 const communeFilePath = __dirname + '/data/communes-dp25.json';
 const codesPostauxFilePath = __dirname + '/data/laposte_hexasmal.json';
+const serialiseDestination = __dirname + '/data/communes.json';
 
-const integration = integrate(communeFilePath, codesPostauxFilePath);
+const integration = integrate(communeFilePath, codesPostauxFilePath, serialiseDestination);
 integration.loadCommunes()
   .then(integration.loadCodePostaux)
   .then(integration.serialize)
