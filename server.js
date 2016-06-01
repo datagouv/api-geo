19fc7d3394e8ce02e34f631abc70110165e44e2f
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-06-01 18:03:29 +0200

    Communes db is now computed on demand

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const { initCommuneFields, initCommuneFormat, formatCommune } = require('./lib/communeHelpers');
-const db = require('./lib/communes');
+const db = require('./lib/communes').getIndexedDb();
 const { pick } = require('lodash');
 
 const app = express();
