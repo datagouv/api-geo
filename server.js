312f942d6d3541531a2f816d52ec2d1bbec5ea52
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-06-01 17:49:32 +0200

    Rename db in communes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const { initCommuneFields, initCommuneFormat, formatCommune } = require('./lib/communeHelpers');
-const db = require('./lib/db');
+const db = require('./lib/communes');
 const { pick } = require('lodash');
 
 const app = express();
