90d2ec1e1df442d8bcfaa3281bf935bf8f81d3ac
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-07-04 15:32:37 +0200

    Load arrondissements w/ geometries and population

--- fetch_data.sh
+++ fetch_data.sh
@@ -5,10 +5,12 @@ mkdir -p data
 echo "Retrieve datasets"
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/laposte_hexasmal.json
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/communes-dp25.json
+wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/osm_arrondissements_municipaux_2016.json
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_cog_france2016.tsv
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_cog_arrond2016.tsv
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_cog_depts2016.tsv
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_cog_reg2016.tsv
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_population_metropole.csv
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_population_dom.csv
+wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_population_arrondissements.csv
 echo "Completed"
