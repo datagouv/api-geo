e0fcff2caac6474eff47c74e1cf19018e2e0a225
tmerlier <tmerlier@student.42.fr>
2017-01-27 20:05:30 +0100

    Manually adds new region names

--- fetch_data.sh
+++ fetch_data.sh
@@ -8,7 +8,8 @@ wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/communes-dp25.json
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_cog_france2016.tsv
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_cog_arrond2016.tsv
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_cog_depts2016.tsv
-wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_cog_reg2016.tsv
+# Awaiting the update of the file planned for April
+# wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_cog_reg2016.tsv
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_population_metropole.csv
 wget -N -P data/ $GEOAPI_DATA_BASE_URL/communes/insee_population_dom.csv
 echo "Completed"
