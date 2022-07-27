dd1665b4dfaa32efbca28138a02b4fd2aa25d921
Thomas Gratier <thomas_gratier@yahoo.fr>
2022-07-27 16:56:50 +0200

    Add entry to build EPCI data for the API

--- build/index.js
+++ build/index.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node --max_old_space_size=4096
 const {buildCommunes} = require('./communes')
+const {buildEpcis} = require('./epcis')
 const {buildDepartements} = require('./departements')
 const {buildRegions} = require('./regions')
 
 async function main() {
   await buildCommunes()
+  await buildEpcis()
   await buildDepartements()
   await buildRegions()
 }
