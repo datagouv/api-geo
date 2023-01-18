8cf1afa41f4420bc7b4a893c729233baf3ca32da
Thomas Gratier <thomas_gratier@yahoo.fr>
2023-01-18 11:32:15 +0100

    Add missing file to download

--- download-sources.sh
+++ download-sources.sh
@@ -5,6 +5,7 @@ echo "Retrieve datasets"
 wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/2022/geojson/communes-5m.geojson.gz
 wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/2022/geojson/communes-50m.geojson.gz
 wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/2022/geojson/communes-associees-deleguees-5m.geojson.gz
+wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/2022/geojson/communes-associees-deleguees-50m.geojson.gz
 wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/2022/geojson/chflieux-communes-arrondissements-municipaux.geojson.gz
 wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/2022/geojson/epci-5m.geojson.gz
 wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/2022/geojson/epci-50m.geojson.gz
