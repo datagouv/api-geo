12030316789823deaefe43e15bac1ccac1b6a119
Thomas Gratier <thomas_gratier@yahoo.fr>
2023-11-28 09:32:05 +0100

    Lower sentry tracesSampleRate for production

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if (process.env.SENTRY_DSN) {
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 1.0
+    tracesSampleRate: 0.02
   })
   app.use(Sentry.Handlers.requestHandler())
   // TracingHandler creates a trace for every incoming request
