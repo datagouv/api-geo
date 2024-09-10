32ccbb170c9d359b66dde418c5975919edd3da86
Thomas Gratier <thomas_gratier@yahoo.fr>
2024-09-10 16:52:55 +0200

    Change to generate temporary dockerfile so we can use only one Dockerfile for any commit

---
+++ generate-dockerfile-millesime.sh
@@ -0,0 +1,27 @@
+DOCKER_MILLESIME=${DOCKER_MILLESIME:-2024}
+
+COMMUNES_ASSOCIEES_DELEGUEES=${COMMUNES_ASSOCIEES_DELEGUEES:-YES}
+
+if [ "$DOCKER_MILLESIME" == "2024" ];
+then
+  YEAR=2024
+  GIT_REF="v2.4.0"
+elif [ "$DOCKER_MILLESIME" == "2023" ];
+then
+  YEAR=2023
+  GIT_REF="v2.3.0"
+else
+  YEAR=2024
+fi
+
+cp Dockerfile DockerfileTemp
+if [ -z ${GIT_REF+x} ];
+then
+  echo "No GIT_REF in this case"
+  GIT_REF=temporary
+else
+  echo "GIT_REF set"
+  git checkout $GIT_REF;
+fi
+docker build -f DockerfileTemp -t apigeo:${GIT_REF} .
+
