7a4d12b3db5ce63de54668dab2f4d4ef85199d47
Thomas Gratier <thomas_gratier@yahoo.fr>
2025-04-17 14:55:25 +0200

    Update to use 2025 source for API Decoupage Administratif

--- download-sources.sh
+++ download-sources.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-YEAR=${YEAR:-2024}
+YEAR=${YEAR:-2025}
 COMMUNES_ASSOCIEES_DELEGUEES=${COMMUNES_ASSOCIEES_DELEGUEES:-NO}
 echo "Create data directory"
 mkdir -p data
 echo "Retrieve datasets"
-wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/${YEAR}/geojson/communes-5m.geojson.gz
-wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/${YEAR}/geojson/communes-50m.geojson.gz
+wget -N -P data/ https://files.data.gouv.fr/dinum-datasets-geo/contours-administratifs/${YEAR}/geojson/communes-5m.geojson.gz
+wget -N -P data/ https://files.data.gouv.fr/dinum-datasets-geo/contours-administratifs/${YEAR}/geojson/communes-50m.geojson.gz
 if [ "$COMMUNES_ASSOCIEES_DELEGUEES" != "NO" ]; then
-    wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/${YEAR}/geojson/communes-associees-deleguees-5m.geojson.gz
-    wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/${YEAR}/geojson/communes-associees-deleguees-50m.geojson.gz
+    wget -N -P data/ https://files.data.gouv.fr/dinum-datasets-geo/contours-administratifs/${YEAR}/geojson/communes-associees-deleguees-5m.geojson.gz
+    wget -N -P data/ https://files.data.gouv.fr/dinum-datasets-geo/contours-administratifs/${YEAR}/geojson/communes-associees-deleguees-50m.geojson.gz
 fi
-wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/${YEAR}/geojson/mairies.geojson.gz
-wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/${YEAR}/geojson/epci-5m.geojson.gz
-wget -N -P data/ http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/${YEAR}/geojson/epci-50m.geojson.gz
+wget -N -P data/ https://files.data.gouv.fr/dinum-datasets-geo/contours-administratifs/${YEAR}/geojson/mairies.geojson.gz
+wget -N -P data/ https://files.data.gouv.fr/dinum-datasets-geo/contours-administratifs/${YEAR}/geojson/epci-5m.geojson.gz
+wget -N -P data/ https://files.data.gouv.fr/dinum-datasets-geo/contours-administratifs/${YEAR}/geojson/epci-50m.geojson.gz
 echo "Completed"
