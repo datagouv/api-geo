c1e94f29175218cd792090b646e620d32fe3b758
Thomas Gratier <thomas_gratier@yahoo.fr>
2026-03-10 16:52:17 +0100

    Update data sources to run API Geo with COG 2026 data

--- download-sources.sh
+++ download-sources.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-YEAR=${YEAR:-2025}
+YEAR=${YEAR:-2026}
 COMMUNES_ASSOCIEES_DELEGUEES=${COMMUNES_ASSOCIEES_DELEGUEES:-NO}
 echo "Create data directory"
 mkdir -p data
 echo "Retrieve datasets"
-wget -N -P data/ https://object.data.gouv.fr/contours-administratifs/${YEAR}/geojson/communes-5m.geojson.gz
-wget -N -P data/ https://object.data.gouv.fr/contours-administratifs/${YEAR}/geojson/communes-50m.geojson.gz
+wget -N -P data/ https://contours-administratifs.s3.rbx.io.cloud.ovh.net/${YEAR}/geojson/communes-5m.geojson.gz
+wget -N -P data/ https://contours-administratifs.s3.rbx.io.cloud.ovh.net/${YEAR}/geojson/communes-50m.geojson.gz
 if [ "$COMMUNES_ASSOCIEES_DELEGUEES" != "NO" ]; then
-    wget -N -P data/ https://object.data.gouv.fr/contours-administratifs/${YEAR}/geojson/communes-associees-deleguees-5m.geojson.gz
-    wget -N -P data/ https://object.data.gouv.fr/contours-administratifs/${YEAR}/geojson/communes-associees-deleguees-50m.geojson.gz
+    wget -N -P data/ https://contours-administratifs.s3.rbx.io.cloud.ovh.net/${YEAR}/geojson/communes-associees-deleguees-5m.geojson.gz
+    wget -N -P data/ https://contours-administratifs.s3.rbx.io.cloud.ovh.net/${YEAR}/geojson/communes-associees-deleguees-50m.geojson.gz
 fi
-wget -N -P data/ https://object.data.gouv.fr/contours-administratifs/${YEAR}/geojson/mairies.geojson.gz
-wget -N -P data/ https://object.data.gouv.fr/contours-administratifs/${YEAR}/geojson/epci-5m.geojson.gz
-wget -N -P data/ https://object.data.gouv.fr/contours-administratifs/${YEAR}/geojson/epci-50m.geojson.gz
+wget -N -P data/ https://contours-administratifs.s3.rbx.io.cloud.ovh.net/${YEAR}/geojson/mairies.geojson.gz
+wget -N -P data/ https://contours-administratifs.s3.rbx.io.cloud.ovh.net/${YEAR}/geojson/epci-5m.geojson.gz
+wget -N -P data/ https://contours-administratifs.s3.rbx.io.cloud.ovh.net/${YEAR}/geojson/epci-50m.geojson.gz
 echo "Completed"
